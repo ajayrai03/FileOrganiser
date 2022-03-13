1bc9d82d510e5e55630d1108b35118da8e863d3f
ajayrai03 <ajayrai1012@gmail.com>
2022-03-13 19:27:14 +0530

    all files added

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
-yup
-jkkjk
+// entry point of my command
+let helpFunc=require("./commands/help")
+let inputArr = process.argv.slice(2);
+let command = inputArr[0];
+let path = inputArr[1];
+switch (command) {
+  case "tree": {
+    console.log("tree function called and executed successfuly on path" + path);
+    break;
+  }
+  case "organize": {
+    console.log(
+      "organize function called and executed successfuly on path" + path
+    );
+    break;
+  }
+  case "help": {
+    console.log("help function called and executed successfuly on path" + path);
+    break;
+  }
+  default: {
+    console.log("command not recognized!");
+    break;
+  }
+}
